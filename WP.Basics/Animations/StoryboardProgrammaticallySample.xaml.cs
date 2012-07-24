200536ce6c6bb0b90c19b85694969ea1d2ada15f
eugenedotnet <eugene.chaikin@live.com>
2012-07-24 22:55:01 +0200

    creating storyboard programmatically

---
+++ WP.Basics/Animations/StoryboardProgrammaticallySample.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace WP.Basics.Animations
+{
+    public partial class StoryboardProgrammaticallySample : UserControl
+    {
+        public StoryboardProgrammaticallySample()
+        {
+            InitializeComponent();
+
+            Rectangle rect = new Rectangle
+            {
+                Fill = new SolidColorBrush(Colors.Green),
+                RenderTransform = new TranslateTransform()
+            };
+            LayoutRoot.Children.Add(rect);
+
+            Loaded += (s, e) =>
+                          {
+                              var sb = CreateAndApplyStoryboard(rect); 
+                              sb.Begin();
+                          };
+        }
+
+        public Storyboard CreateAndApplyStoryboard(UIElement targetElement)
+        {
+            Storyboard sb = new Storyboard();
+
+            DoubleAnimation animation = new DoubleAnimation { From = 0, To = 700 };
+
+            Storyboard.SetTarget(animation, targetElement);
+            Storyboard.SetTargetProperty(animation, new PropertyPath("(UIElement.RenderTransform).(TranslateTransform.Y)"));
+
+            sb.Children.Add(animation);
+            return sb;
+        }
+    }
+}
